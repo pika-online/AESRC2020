861d66960cc33a602ee1efdb62fb634d0de328b2
Ephemeroptera <44800504+coolEphemeroptera@users.noreply.github.com>
2020-09-29 16:18:04 +0800

    Update model.py

--- local/model.py
+++ local/model.py
@@ -1,10 +1,19 @@
 from local.resnet import resnet18_,resnet34_,resnet50_,resnet101_,resnet152_
-from keras.layers import Input, Dense, Lambda,Dropout, Reshape,LSTM,Bidirectional,GRU
+from keras.layers import Input, Dense, Lambda,Dropout, Reshape,LSTM,\
+    Bidirectional,GRU,GlobalAveragePooling1D,Layer,AveragePooling1D
+from keras.layers.cudnn_recurrent import CuDNNGRU,CuDNNLSTM
 from keras.models import Model
 from keras import backend as K
 from keras.regularizers import l2
 from keras.utils import multi_gpu_model
 from keras.optimizers import Adam
+from keras_self_attention import SeqSelfAttention
+from keras_multi_head import MultiHeadAttention
+import local.transformer as tfr
+
+"""
+
+"""
 
 
 """
@@ -23,7 +32,7 @@ def ctc_input_length(tensor):
 
 """
 =========================
-        models
+        CTC Models
 =========================
 """
 
@@ -47,9 +56,9 @@ def model_ctc(shapes,
         """
 
     # shared-layer
-    cnn1 = resnet18_(shapes)
+    # cnn1 = resnet18_(shapes)
     # cnn1 = resnet34_(shapes)
-    # cnn1 = resnet50_(shapes)
+    cnn1 = resnet50_(shapes)
     # cnn1 = resnet101_(shapes)
     # cnn1 = resnet152_(shapes)
     encoder_len = int(cnn1.output.shape[1] * cnn1.output.shape[2])
@@ -119,9 +128,9 @@ def model_ctc_accent(shapes,
     # shared-layer
     cnn1 = resnet18_(shapes)
     # cnn1 = resnet34_(shapes)
-    # cnn1 = resnet50_(shapes)
+    # cnn1 = resnet52_(shapes)
     # cnn1 = resnet101_(shapes)
-    # cnn1 = resnet152_(shapes)
+    # cnn1 = resnet152_()_(shapes)
     encoder_len = int(cnn1.output.shape[1]*cnn1.output.shape[2])
     print("==== the model encoder length is: %d ===="%encoder_len)
     print("==== Warnning: you should let max_label_len <= encoder_len ====")
@@ -171,8 +180,292 @@ def model_ctc_accent(shapes,
 
 
 """
+====================================
+        Attention-CTC Models
+====================================
+"""
+
+def self_att(x):
+    return SeqSelfAttention(
+        units=128,
+        attention_width=20,
+        attention_type=SeqSelfAttention.ATTENTION_TYPE_MUL,
+        attention_activation='relu',
+        kernel_regularizer=l2(1e-6),)(x)
+
+
+def model_att_ctc(shapes,
+                  bpe_classes,
+                  max_label_len,
+                  raw_model=None
+                  ):
+    """
+        Params:
+            <shape>: the shape of input tensor
+            <bpe_classes>: the size of bpe set
+            <max_label_len>: maximum labels length (for ctc)
+            <raw_model>: init weights
+        Return:
+            <model>: ctc models
+        Model:
+            inputs: [inputs, ctc_labels, ctc_input_len,ctc_label_len]
+            outputs: [ctc_loss]
+
+        """
+
+    # shared-layer
+    # cnn1 = resnet18_(shapes)
+    cnn1 = resnet34_(shapes)
+    # cnn1 = resnet50_(shapes)
+    # cnn1 = resnet101_(shapes)
+    # cnn1 = resnet152_(shapes)
+    encoder_len = int(cnn1.output.shape[1] * cnn1.output.shape[2])
+    print("==== the model encoder length is: %d ====" % encoder_len)
+    print("==== Warnning: you should let max_label_len <= encoder_len ====")
+    cnn2 = Reshape((encoder_len, int(cnn1.output.shape[3])))
+    cnn3 = Dense(256, activation="relu", kernel_initializer='he_normal', name="cnn")
+
+    # INPUTS
+    inputs = Input(shapes, name="inputs")
+
+    # ENCODER
+    encoder = cnn1(inputs)
+    encoder = cnn2(encoder)
+    encoder = cnn3(encoder)
+    encoder = self_att(encoder)
+    encoder = Dropout(0.3)(encoder)
+
+    # CTC
+    ctc = Dense(256, activation="relu", kernel_initializer='he_normal', name='ctc')(encoder)
+    ctc = Dropout(0.3)(ctc)
+    ctc_pred = Dense(bpe_classes, activation="softmax", kernel_initializer='he_normal', name='ctc_pred')(ctc)
+    ctc_input_len = Input(shape=[1], dtype='int32', name='ctc_input_len')
+    ctc_label_len = Input(shape=[1], dtype='int32', name='ctc_label_len')
+    ctc_labels = Input([max_label_len], dtype='float32', name='ctc_labels')
+    ctc_loss = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc_loss')(
+        [ctc_pred, ctc_labels, ctc_input_len, ctc_label_len])
+
+
+    # MODEL
+    model = Model(inputs=[inputs, ctc_labels, ctc_input_len, ctc_label_len],
+                  outputs=[ctc_loss], name="model_ctc")
+    model.summary()
+    if raw_model:
+        print("===== init weights from:%s =====" % raw_model)
+        model.load_weights(raw_model, by_name=True, skip_mismatch=True)
+
+    return model
+
+
+def model_tfr_ctc(shapes,
+                  bpe_classes,
+                  max_label_len,
+                  raw_model=None
+                  ):
+    """
+        Params:
+            <shape>: the shape of input tensor
+            <bpe_classes>: the size of bpe set
+            <max_label_len>: maximum labels length (for ctc)
+            <raw_model>: init weights
+        Return:
+            <model>: ctc models
+        Model:
+            inputs: [inputs, ctc_labels, ctc_input_len,ctc_label_len]
+            outputs: [ctc_loss]
+
+        """
+    # shared-layer
+    cnn1 = resnet18_(shapes)
+    # cnn1 = resnet34_(shapes)
+    # cnn1 = resnet50_(shapes)
+    # cnn1 = resnet101_(shapes)
+    # cnn1 = resnet152_(shapes)
+    encoder_len = int(cnn1.output.shape[1] * cnn1.output.shape[2])
+    print("==== the model encoder length is: %d ====" % encoder_len)
+    print("==== Warnning: you should let max_label_len <= encoder_len ====")
+    cnn2 = Reshape((encoder_len, int(cnn1.output.shape[3])))
+    cnn3 = Dense(256, activation="relu", kernel_initializer='he_normal', name="cnn")
+
+    # INPUTS
+    inputs = Input(shapes, name="inputs")
+
+    # ENCODER
+    encoder = cnn1(inputs)
+    encoder = cnn2(encoder)
+    encoder = cnn3(encoder)
+    encoder = tfr.EncoderLayer(d_model=256,d_inner_hid=1024,n_head=8)(encoder)
+    encoder = Dropout(0.3)(encoder)
+
+    # CTC
+    ctc = Dense(256, activation="relu", kernel_initializer='he_normal', name='ctc')(encoder)
+    ctc = Dropout(0.3)(ctc)
+    ctc_pred = Dense(bpe_classes, activation="softmax", kernel_initializer='he_normal', name='ctc_pred')(ctc)
+    ctc_input_len = Input(shape=[1], dtype='int32', name='ctc_input_len')
+    ctc_label_len = Input(shape=[1], dtype='int32', name='ctc_label_len')
+    ctc_labels = Input([max_label_len], dtype='float32', name='ctc_labels')
+    ctc_loss = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc_loss')(
+        [ctc_pred, ctc_labels, ctc_input_len, ctc_label_len])
+
+
+    # MODEL
+    model = Model(inputs=[inputs, ctc_labels, ctc_input_len, ctc_label_len],
+                  outputs=[ctc_loss], name="model_tfr_ctc")
+    model.summary()
+    if raw_model:
+        print("===== init weights from:%s =====" % raw_model)
+        model.load_weights(raw_model, by_name=True, skip_mismatch=True)
+
+    return model
+
+
+
+
+def model_att_ctc_accent(shapes,
+                     bpe_classes,
+                     accent_classes,
+                     max_label_len,
+                     raw_model=None
+                     ):
+    """
+    Params:
+        <shape>: the shape of input tensor
+        <bpe_classes>: the size of bpe set
+        <accent_classes>: the size of accents
+        <max_label_len>: maximum labels length (for ctc)
+        <raw_model>: init weights
+    Return:
+        <model>: ctc-accents multi-task-learning models
+    Model:
+        inputs: [inputs, ctc_labels, ctc_input_len,ctc_label_len]
+        outputs: [ctc_loss, accent_labels]
+
+    """
+
+    # shared-layer
+    cnn1 = resnet18_(shapes)
+    # cnn1 = resnet34_(shapes)
+    # cnn1 = resnet52_(shapes)
+    # cnn1 = resnet101_(shapes)
+    # cnn1 = resnet152_()_(shapes)
+    encoder_len = int(cnn1.output.shape[1]*cnn1.output.shape[2])
+    print("==== the model encoder length is: %d ===="%encoder_len)
+    print("==== Warnning: you should let max_label_len <= encoder_len ====")
+    cnn2 = Reshape((encoder_len,int(cnn1.output.shape[3])))
+    cnn3 = Dense(256, activation="relu", kernel_initializer='he_normal', name="cnn")
+
+    # INPUTS
+    inputs = Input(shapes, name="inputs")
+
+    # ENCODER
+    encoder = cnn1(inputs)
+    encoder = cnn2(encoder)
+    encoder = cnn3(encoder)
+    encoder = self_att(encoder)
+    encoder = Dropout(0.3)(encoder)
+
+    # CTC
+    ctc = Dense(256, activation="relu", kernel_initializer='he_normal', name='ctc')(encoder)
+    ctc = Dropout(0.3)(ctc)
+    ctc_pred = Dense(bpe_classes, activation="softmax", kernel_initializer='he_normal',name='ctc_pred')(ctc)
+    ctc_input_len = Input(shape=[1], dtype='int32', name='ctc_input_len')
+    ctc_label_len = Input(shape=[1], dtype='int32', name='ctc_label_len')
+    ctc_labels = Input([max_label_len], dtype='float32', name='ctc_labels')
+    ctc_loss = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc_loss')([ctc_pred, ctc_labels, ctc_input_len, ctc_label_len])
+
+    # ACCENT
+    accents = GlobalAveragePooling1D()(encoder)
+    accents = Dense(256, activation="relu", kernel_initializer='he_normal')(accents)
+    accents = Dropout(0.3)(accents)
+    aesrc_nation = Dense(accent_classes, activation='softmax', kernel_initializer='he_normal',name="accent_labels")(accents)
+
+    # MODEL
+    model = Model(inputs=[inputs, ctc_labels, ctc_input_len, ctc_label_len],
+                  outputs=[ctc_loss, aesrc_nation ], name="model_tfr_ctc_accents")
+    model.summary()
+    if raw_model:
+        print("===== init weights from:%s =====" % raw_model)
+        model.load_weights(raw_model, by_name=True,skip_mismatch=True)
+
+    return model
+
+
+
+
+def model_tfr_ctc_accent(shapes,
+                     bpe_classes,
+                     accent_classes,
+                     max_label_len,
+                     raw_model=None
+                     ):
+    """
+    Params:
+        <shape>: the shape of input tensor
+        <bpe_classes>: the size of bpe set
+        <accent_classes>: the size of accents
+        <max_label_len>: maximum labels length (for ctc)
+        <raw_model>: init weights
+    Return:
+        <model>: ctc-accents multi-task-learning models
+    Model:
+        inputs: [inputs, ctc_labels, ctc_input_len,ctc_label_len]
+        outputs: [ctc_loss, accent_labels]
+
+    """
+
+    # shared-layer
+    cnn1 = resnet18_(shapes)
+    # cnn1 = resnet34_(shapes)
+    # cnn1 = resnet52_(shapes)
+    # cnn1 = resnet101_(shapes)
+    # cnn1 = resnet152_()_(shapes)
+    encoder_len = int(cnn1.output.shape[1]*cnn1.output.shape[2])
+    print("==== the model encoder length is: %d ===="%encoder_len)
+    print("==== Warnning: you should let max_label_len <= encoder_len ====")
+    cnn2 = Reshape((encoder_len,int(cnn1.output.shape[3])))
+    cnn3 = Dense(256, activation="relu", kernel_initializer='he_normal', name="cnn")
+
+    # INPUTS
+    inputs = Input(shapes, name="inputs")
+
+    # ENCODER
+    encoder = cnn1(inputs)
+    encoder = cnn2(encoder)
+    encoder = cnn3(encoder)
+    encoder = tfr.EncoderLayer(d_model=256, d_inner_hid=1024, n_head=8)(encoder)
+    encoder = Dropout(0.3)(encoder)
+
+    # CTC
+    ctc = Dense(256, activation="relu", kernel_initializer='he_normal', name='ctc')(encoder)
+    ctc = Dropout(0.3)(ctc)
+    ctc_pred = Dense(bpe_classes, activation="softmax", kernel_initializer='he_normal',name='ctc_pred')(ctc)
+    ctc_input_len = Input(shape=[1], dtype='int32', name='ctc_input_len')
+    ctc_label_len = Input(shape=[1], dtype='int32', name='ctc_label_len')
+    ctc_labels = Input([max_label_len], dtype='float32', name='ctc_labels')
+    ctc_loss = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc_loss')([ctc_pred, ctc_labels, ctc_input_len, ctc_label_len])
+
+    # ACCENT
+    accents = GlobalAveragePooling1D()(encoder)
+    accents = Dense(256, activation="relu", kernel_initializer='he_normal')(accents)
+    accents = Dropout(0.3)(accents)
+    aesrc_nation = Dense(accent_classes, activation='softmax', kernel_initializer='he_normal',name="accent_labels")(accents)
+
+    # MODEL
+    model = Model(inputs=[inputs, ctc_labels, ctc_input_len, ctc_label_len],
+                  outputs=[ctc_loss, aesrc_nation ], name="model_tfr_ctc_accents")
+    model.summary()
+    if raw_model:
+        print("===== init weights from:%s =====" % raw_model)
+        model.load_weights(raw_model, by_name=True,skip_mismatch=True)
+
+    return model
+
+
+
+
+"""
 =========================
-       compile models
+       combile models
 =========================
 """
 def compile(model,
@@ -210,6 +503,12 @@ def ctc_pred(model,x,batch_size,input_len,):
 
 if __name__=="__main__":
 
-    model = model_ctc(shapes=[1200,80,1],
+    model = model_tfr_ctc(shapes=[1200,80,1],
                              bpe_classes=1000,
                              max_label_len=72)
+    # model = model_tfr_ctc_accent(shapes=[1200, 80, 1],
+    #                             bpe_classes=1000,
+    #                             max_label_len=72,
+    #                              accent_classes=8)
+    model.save_weights('../exp/demo.h5')
+    model.load_weights('../exp/demo.h5')
