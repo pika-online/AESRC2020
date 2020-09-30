d5426ec8844fc957db035f6c6507837a2f65c1e7
Ephemeroptera <44800504+coolEphemeroptera@users.noreply.github.com>
2020-09-30 14:02:08 +0800

    Update train_libri.py

--- train_libri.py
+++ train_libri.py
@@ -10,10 +10,11 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
 def train():
 
-    model = mdl.model_ctc( shapes=(MAX_INPUT_LEN, FEAT_DIM, 1),
-                                          bpe_classes=BPE_CLASSES,
-                                          max_label_len=MAX_LABEL_LEN,
-                                          raw_model=RAW_MODEL)
+    # RES + RNN
+    model = mdl.model_res_gru_ctc(shapes=(MAX_INPUT_LEN, FEAT_DIM, 1),
+                                  bpe_classes=BPE_CLASSES,
+                                  max_label_len=MAX_LABEL_LEN,
+                                  raw_model=RAW_MODEL)
 
     parallel_model = mdl.compile(model,gpus=3,lr=0.001,
                                  loss=[lambda y_true, y_pred: y_pred],
@@ -42,7 +43,60 @@ def train():
                                  validation_data=(dev_data[0], dev_data[1]), validation_steps=N_BATCHS)
 
 
+def test():
 
+    model_file = "/disc1/ARNet/exp/libri/res_gru/012.h5"
+
+    FEATS_DEV_CLEAN = us.kaldiio.load_scp(dev_clean_file)
+    FEATS_DEV_OTHER = us.kaldiio.load_scp(dev_other_file)
+    FEATS_TEST_CLEAN = us.kaldiio.load_scp(test_clean_file)
+    FEATS_TEST_OTHER = us.kaldiio.load_scp(test_other_file)
+
+    dev_clean_lst = us.scp2key(us.read_lines(dev_clean_file))
+    dev_other_lst = us.scp2key(us.read_lines(dev_other_file))
+    test_clean_lst = us.scp2key(us.read_lines(test_clean_file))
+    test_other_lst = us.scp2key(us.read_lines(test_other_file))
+
+    dev_clean_data = us.load_ctc(dev_clean_lst, FEATS_DEV_CLEAN,
+                           encoder_len=ENCODER_LEN,
+                           max_input_len=MAX_INPUT_LEN,
+                           max_label_len=MAX_LABEL_LEN,
+                           trans_ids=us.LIBRI_TRANS_IDS)
+    dev_other_data = us.load_ctc(dev_other_lst, FEATS_DEV_OTHER,
+                                 encoder_len=ENCODER_LEN,
+                                 max_input_len=MAX_INPUT_LEN,
+                                 max_label_len=MAX_LABEL_LEN,
+                                 trans_ids=us.LIBRI_TRANS_IDS)
+    test_clean_data = us.load_ctc(test_clean_lst, FEATS_TEST_CLEAN,
+                                 encoder_len=ENCODER_LEN,
+                                 max_input_len=MAX_INPUT_LEN,
+                                 max_label_len=MAX_LABEL_LEN,
+                                 trans_ids=us.LIBRI_TRANS_IDS)
+    test_other_data = us.load_ctc(test_other_lst, FEATS_TEST_OTHER,
+                                 encoder_len=ENCODER_LEN,
+                                 max_input_len=MAX_INPUT_LEN,
+                                 max_label_len=MAX_LABEL_LEN,
+                                 trans_ids=us.LIBRI_TRANS_IDS)
+
+    model = mdl.model_res_gru_ctc(shapes=(MAX_INPUT_LEN, FEAT_DIM, 1),
+                                         bpe_classes=BPE_CLASSES,
+                                         max_label_len=MAX_LABEL_LEN,
+                                         raw_model=model_file)
+
+    ctc_decode_model = mdl.sub_model(model, 'inputs', 'ctc_pred')
+    dev_clean_pred = mdl.ctc_pred(ctc_decode_model, dev_clean_data[0], input_len=ENCODER_LEN, batch_size=BATCH_SIZE)
+    dev_other_pred = mdl.ctc_pred(ctc_decode_model, dev_other_data[0], input_len=ENCODER_LEN, batch_size=BATCH_SIZE)
+    test_clean_pred = mdl.ctc_pred(ctc_decode_model, test_clean_data[0], input_len=ENCODER_LEN, batch_size=BATCH_SIZE)
+    test_other_pred = mdl.ctc_pred(ctc_decode_model, test_other_data[0], input_len=ENCODER_LEN, batch_size=BATCH_SIZE)
+
+    print("dev-clean-wer:",
+          us.ctc_eval(dev_clean_data[0]["ctc_labels"],dev_clean_data[0]["ctc_label_len"], dev_clean_pred, True))
+    print("dev-other-wer:",
+          us.ctc_eval(dev_other_data[0]["ctc_labels"], dev_other_data[0]["ctc_label_len"], dev_other_pred, True))
+    print("test-clean-wer:",
+          us.ctc_eval(test_clean_data[0]["ctc_labels"], test_clean_data[0]["ctc_label_len"], test_clean_pred, True))
+    print("test-other-wer:",
+          us.ctc_eval(test_other_data[0]["ctc_labels"], test_other_data[0]["ctc_label_len"], test_other_pred, True))
 
 
 if __name__ == "__main__":
@@ -51,33 +105,37 @@ if __name__ == "__main__":
     MAX_INPUT_LEN = 1600
     MAX_LABEL_LEN = 100
     ENCODER_LEN = 150
-    BATCH_SIZE = 300
-    INIT_EPOCH = 2
+    BATCH_SIZE = 150
+    INIT_EPOCH = 6
     FEAT_DIM = 80
     BPE_CLASSES = 1000
-    EPOCHS = 30
-    RAW_MODEL = '/disc1/ARNet/exp/aesrc/002.h5'
-    LOG_FILE = 'exp/libri/model.csv'
-    MODEL_DIR = "exp/libri"
+    EPOCHS = 20
+    CNN = 'res34'
+    RAW_MODEL = None
+    LOG_FILE = 'exp/libri/res34_gru/model.csv'
+    MODEL_DIR = "exp/libri/res34_gru/"
 
     # file
     train_file = "/disc1/AESRC2020/data/librispeech_train/feats.scp"
-    dev_file = "/disc1/AESRC2020/data/librispeech_dev_clean/feats.scp"
+    dev_clean_file = "/disc1/AESRC2020/data/librispeech_dev_clean/feats.scp"
+    dev_other_file = "/disc1/AESRC2020/data/librispeech_dev_other/feats.scp"
+    test_clean_file = "/disc1/AESRC2020/data/librispeech_test_clean/feats.scp"
+    test_other_file = "/disc1/AESRC2020/data/librispeech_test_other/feats.scp"
 
     # feats
     FEATS = us.kaldiio.load_scp(train_file)
-    FEATS_DEV = us.kaldiio.load_scp(dev_file)
+    FEATS_DEV = us.kaldiio.load_scp(dev_clean_file)
 
     # list
     train_lst = us.limit_time_utts(us.limit_trans_utts(us.scp2key(us.read_lines(train_file)),
                                     us.LIBRI_TRANS_IDS, MAX_LABEL_LEN), us.LIBRI_UTT2FRAMES, MAX_INPUT_LEN)
-    dev_lst = us.scp2key(us.read_lines(dev_file))
+    dev_lst = us.scp2key(us.read_lines(dev_clean_file))
     N_BATCHS = len(train_lst) // BATCH_SIZE
 
 
     # callbacks
-    lr_reducer = ReduceLROnPlateau(factor=0.5, cooldown=0, patience=1, min_lr=0.5e-6,
-                                   monitor='val_loss',min_delta=0.1,verbose=1)
+    lr_reducer = ReduceLROnPlateau(factor=0.5, cooldown=0, patience=0, min_lr=0.5e-6,
+                                   monitor='val_loss',min_delta=1.0,verbose=1)
     early_stopper = EarlyStopping( min_delta=0.01, patience=3,monitor='val_loss',verbose=1)
     csv_logger = CSVLogger(LOG_FILE)
     # checkpointer = ModelCheckpoint(filepath=MODEL_FILE, verbose=1, save_best_only=True, monitor='val_loss',
@@ -99,7 +157,8 @@ if __name__ == "__main__":
                            trans_ids=us.LIBRI_TRANS_IDS)
 
 
-    train()
+    # train()
+    test()
     exit()
 
 
