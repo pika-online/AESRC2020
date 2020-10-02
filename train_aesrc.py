fa04e0fd97a072169ffb84a3edc22556402dbdb8
Ephemeroptera <44800504+coolEphemeroptera@users.noreply.github.com>
2020-10-02 13:02:58 +0800

    Update train_aesrc.py

--- train_aesrc.py
+++ train_aesrc.py
@@ -10,13 +10,16 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
 def train():
 
-    model = mdl.model_ctc_accent( shapes=(MAX_INPUT_LEN, FEAT_DIM, 1),
+    model = mdl.model_res_gru_ctc_accent( shapes=(MAX_INPUT_LEN, FEAT_DIM, 1),
                                         accent_classes=ACCENT_CLASSES,
                                           bpe_classes=BPE_CLASSES,
                                           max_label_len=MAX_LABEL_LEN,
+                                          cnn=CNN,
                                           raw_model=RAW_MODEL)
 
-    parallel_model = mdl.compile(model,gpus=3,lr=0.0001,
+
+
+    parallel_model = mdl.compile(model,gpus=4,lr=0.0005,
                                  loss=[lambda y_true, y_pred: y_pred,"categorical_crossentropy"],
                                  loss_weights=[0.4,0.6],
                                  metrics={"accent_labels": 'accuracy'})
@@ -57,15 +60,16 @@ if __name__ == "__main__":
     MAX_INPUT_LEN = 1200
     MAX_LABEL_LEN = 72
     ENCODER_LEN = 114
-    BATCH_SIZE = 300
-    INIT_EPOCH = 4
+    BATCH_SIZE = 128
+    INIT_EPOCH = 2
     FEAT_DIM = 80
     BPE_CLASSES = 1000
     ACCENT_CLASSES = 8
-    EPOCHS = 30
-    RAW_MODEL = '/disc1/ARNet/exp/aesrc/003.h5'
-    LOG_FILE = 'exp/aesrc/model.csv'
-    MODEL_DIR = "exp/aesrc"
+    EPOCHS = 20
+    CNN = 'res18'
+    RAW_MODEL = '/disc1/ARNet/exp/aesrc/res18_gru/001.h5'
+    LOG_FILE = '/disc1/ARNet/exp/aesrc/res18_gru/model.csv'
+    MODEL_DIR = '/disc1/ARNet/exp/aesrc/res18_gru/'
 
     # file
     train_file = "/disc1/AESRC2020/data/aesrc_fbank_sp/train.scp"
@@ -84,9 +88,10 @@ if __name__ == "__main__":
 
 
     # callbacks
-    lr_reducer = ReduceLROnPlateau(factor=0.5, cooldown=0, patience=1, min_lr=0.5e-6,
-                                   monitor='accent_labels_loss',min_delta=0.1,verbose=1)
-    early_stopper = EarlyStopping( min_delta=0.01, patience=3,monitor='accent_labels_loss',verbose=1)
+    lr_reducer = ReduceLROnPlateau(factor=0.5, cooldown=0, patience=0, min_lr=0.5e-6,
+                                   monitor='accent_labels_acc',mode='max',min_delta=0.5,verbose=1)
+    early_stopper = EarlyStopping( patience=3,
+                                   monitor='accent_labels_acc',mode='max',min_delta=0.1,verbose=1)
     csv_logger = CSVLogger(LOG_FILE)
     # checkpointer = ModelCheckpoint(filepath=MODEL_FILE, verbose=1, save_best_only=True, monitor='val_loss',
     #                                save_weights_only=True)
